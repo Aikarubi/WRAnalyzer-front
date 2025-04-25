b42405f1453fa5f109a9179fa54b78351ec8468d
Aikarubi <nerupe@floridauniversitaria.es>
2025-04-25 13:38:23 +0200

    seccion 1 estructurada y con estilos añadidos

--- wrAnalyzer/src/app/app.routes.ts
+++ wrAnalyzer/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { HomeComponent } from './views/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: MainLayoutComponent,
+        children: [
+            { path: '', component: HomeComponent },
+        ]
+    }
+];
--- wrAnalyzer/src/app/views/home/home.component.ts
+++ wrAnalyzer/src/app/views/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { Seccion1Component } from '../../components/seccion1/seccion1.component';
+import { Seccion2Component } from '../../components/seccion2/seccion2.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [Seccion1Component, Seccion2Component],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
