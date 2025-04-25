6ca8a02fe3024ef511179fdb71608cc8af2fa128
Aikarubi <nerupe@floridauniversitaria.es>
2025-04-25 12:02:24 +0200

    añadiendo componentes y vistas

---
+++ wrAnalyzer/src/app/views/analyzer/analyzer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analyzer',
+  standalone: true,
+  imports: [],
+  templateUrl: './analyzer.component.html',
+  styleUrl: './analyzer.component.css'
+})
+export class AnalyzerComponent {
+
+}
