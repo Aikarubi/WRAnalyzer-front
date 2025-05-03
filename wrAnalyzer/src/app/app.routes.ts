621313744b47454fddd696898aaf31824e03d72a
Aikarubi <nerupe@floridauniversitaria.es>
2025-05-03 11:03:33 +0200

    ruta añadida

--- wrAnalyzer/src/app/app.routes.ts
+++ wrAnalyzer/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 import { HomeComponent } from './views/home/home.component';
+import { AnalyzerLayoutComponent } from './layouts/analyzer-layout/analyzer-layout.component';
+import { AnalyzerComponent } from './views/analyzer/analyzer.component';
 
 export const routes: Routes = [
 
@@ -9,6 +11,13 @@ export const routes: Routes = [
         component: MainLayoutComponent,
         children: [
             { path: '', component: HomeComponent },
+        ]
+    },
+    {
+        path: 'analyzer',
+        component: AnalyzerLayoutComponent,
+        children: [
+            { path: '', component: AnalyzerComponent },
         ]
     }
 ];
