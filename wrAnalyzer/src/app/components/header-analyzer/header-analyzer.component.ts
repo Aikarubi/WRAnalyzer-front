075c75927f2b985eb343f9952b36725341f0df9a
Aikarubi <nerupe@floridauniversitaria.es>
2025-05-06 11:01:08 +0200

    ajustando rutas

--- wrAnalyzer/src/app/components/header-analyzer/header-analyzer.component.ts
+++ wrAnalyzer/src/app/components/header-analyzer/header-analyzer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header-analyzer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header-analyzer.component.html',
   styleUrl: './header-analyzer.component.css'
 })
